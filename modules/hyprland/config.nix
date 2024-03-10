5916429852801bd0ffaa978e8b312c2e07bf15be
styrix560 <guennie.pizzabauer@gmail.com>
2024-03-10 13:46:57 +0100

    62   2024-03-10 13:46:54   (current)

--- modules/hyprland/config.nix
+++ modules/hyprland/config.nix
@@ -74,14 +74,7 @@
 
       bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
 
-      animation = [
-        "windows, 1, 7, default"
-        "windowsOut, 1, 7, default popin 80%"
-        "border, 1, 10, default"
-        "borderangle, 1, 8, default"
-        "fade, 1, 7, default"
-        "workspaces, 1, 6, default"
-      ];
+      animation = [];
     };
 
     dwindle = {
