d40a331493c34790a8ec8ac76a688737ebecabeb
styrix560 <guennie.pizzabauer@gmail.com>
2024-03-17 16:49:37 +0100

    98   2024-03-17 16:49:34   (current)

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,39 @@
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
+  programs.hyprland.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session.command = ''
+        ${pkgs.greetd.tuigreet}/bin/tuigreet \
+                    --time \
+               --asterisks \
+               --user-menu \
+               --cmd 'Hyprland'
+      '';
+    };
+  };
+
+  environment.etc."greetd/environments".text = ''
+    Hyprland
+  '';
+  home-manager = {
+    extraSpecialArgs = {inherit inputs;};
+    users = {
+      "admin" = import ./home.nix;
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
   environment.systemPackages = with pkgs; [
     # npm
     nodejs_21
@@ -153,42 +186,12 @@
 
     # resource monitor
     pkgs.btop
+
+    # file manager
+    pkgs.xfce.thunar
   ];
 
   fonts.packages = with pkgs; [
     pkgs.font-awesome
   ];
-
-  programs.hyprland.enable = true;
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session.command = ''
-        ${pkgs.greetd.tuigreet}/bin/tuigreet \
-                    --time \
-               --asterisks \
-               --user-menu \
-               --cmd 'Hyprland'
-      '';
-    };
-  };
-
-  environment.etc."greetd/environments".text = ''
-    Hyprland
-  '';
-  home-manager = {
-    extraSpecialArgs = {inherit inputs;};
-    users = {
-      "admin" = import ./home.nix;
-    };
-  };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
 }
