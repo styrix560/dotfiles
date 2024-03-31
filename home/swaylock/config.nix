2bc2c34916cab542f4beb823324de5895cd2594b
styrix560 <guennie.pizzabauer@gmail.com>
2024-03-31 14:46:16 +0200

    179   2024-03-31 14:46:11   (current)

--- home/swaylock/config.nix
+++ home/swaylock/config.nix
@@ -2,7 +2,7 @@
   programs.swaylock = {
     enable = true;
     settings = {
-      color = "000000";
+      color = "222222";
       show-failed-attempts = true;
       ignore-empty-password = true;
       show-keyboard-layout = true;
