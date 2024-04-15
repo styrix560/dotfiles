859de5f839bd57b705677300950183e228b2b8d9
styrix560 <guennie.pizzabauer@gmail.com>
2024-04-15 21:13:21 +0200

    192   2024-04-15 21:13:18   (current)

--- configuration.nix
+++ configuration.nix
@@ -129,7 +129,6 @@ in {
       enable = true;
       luaModules = with pkgs.luaPackages; [
         luarocks # is the package manager for Lua modules
-        luadbi-mysql # Database abstraction layer
       ];
     };
   };
